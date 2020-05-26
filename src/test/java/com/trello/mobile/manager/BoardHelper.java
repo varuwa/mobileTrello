17b9eecc2c7575f7cecf8d57feebaef4e46dc3cb
varuwa <varuwa@gmail.com>
2020-05-26 15:16:10 +0300

    added getBoardsList

--- src/test/java/com/trello/mobile/manager/BoardHelper.java
+++ src/test/java/com/trello/mobile/manager/BoardHelper.java
@@ -3,6 +3,9 @@ package com.trello.mobile.manager;
 import com.trello.mobile.tests.TestBase;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class BoardHelper extends HelperBase {
     public BoardHelper(AppiumDriver driver) {
@@ -16,5 +19,14 @@ public class BoardHelper extends HelperBase {
     public void swipeBoardsPageUp() {
         swipeUp();
     }
+
+    public void getBoardsList(){
+        List<WebElement> boards = driver.findElements(By.xpath("//*[@resource-id='com.trello:id/board_name']"));
+        for(WebElement board : boards){
+            String name = board.getText();
+            System.out.println(name);
+        }
+    }
+
 
 }
--- src/test/java/com/trello/mobile/tests/BoardsNameTest.java
+++ src/test/java/com/trello/mobile/tests/BoardsNameTest.java
@@ -1,18 +1,33 @@
 package com.trello.mobile.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BoardsNameTest extends TestBase {
     @Test
     public void getBoardsName(){
         app.getSession().login("varuwa@gmail.com", "trellobarbara");
-        for(int i = 0; i < 100; i++){
-            String boardName = app.getBoard().getNameOfBoard();
-            System.out.println(boardName);
+
+        int count = 0;
+        do {
+            app.getBoard().getBoardsList();
             app.getBoard().swipeBoardsPageUp();
+            count++;
         }
+        while (count == 5);
+
+//        for(int i = 0; i < 8; i++) {
+//            app.getBoard().getBoardsList();
+//            if(i == 7){
+//                app.getBoard().swipeBoardsPageUp();
+//                i = 0;
+//            }
+//        }
 
     }
+
 }
